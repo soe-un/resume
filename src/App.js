0555caa99a4294fe614854e3a7dfe2af1c38d827
soe-un <runterf@labradorlabs.ai>
2024-10-19 01:16:39 +0900

    resume #2

--- src/App.js
+++ src/App.js
@@ -42,15 +42,12 @@ function App() {
       <div className="content-wrap">
         <div className="content">
           <h3>About Me… </h3>
-          <p>스타트업에서 근무하는 3년차 프론트엔드 개발자입니다.</p>
+          <p>스타트업에서 근무하는 3년차 웹 프론트엔드 개발자입니다.</p>
           <p>
             업무에 도움이 되는 일을 생각하고,
             <strong>비즈니스의 궁극적인 지향점</strong>을 찾기 위해 노력합니다.
           </p>
-          <p>
-            웹 서비스를 기획/개발/배포/운영한 경험과 5명 규모의 팀에서 일정 관리
-            및 업무 분배를 한 경험이 있습니다.
-          </p>
+          <p>5명 규모의 팀에서 일정 관리 및 업무 분배를 한 경험이 있습니다.</p>
           <p>불편함을 줄이기 위해 자동화 기술을 도입하는 것을 좋아합니다.</p>
         </div>
         <div className="thin-divider" />
@@ -71,21 +68,22 @@ function App() {
             <a href="https://labradorlabs.ai" target="_blank">
               https://labradorlabs.ai
             </a>
-            <p>Frontend Developer</p>
-            <p>2021.06 ~ 현재</p>
+            <p>Frontend Developer / Manager</p>
+            <p>2021.06 - 현재</p>
           </div>
           <div className="sub-content">
-            <h5>래브라도 솔루션 프론트엔드 담당자</h5>
+            <h5>래브라도 프론트엔드 담당자</h5>
             <p>
-              오픈소스 취약점, 라이선스를 분석하기 위한 래브라도 솔루션에서 기존
-              코드를 리팩토링하고 새로운 기능을 개발하고 있습니다.
+              제품의 SBOM, 취약점, 라이선스를 분석하기 위한 솔루션 래브라도에서
+              <br />
+              기존 코드를 리팩토링하고 새로운 기능을 개발하고 있습니다.
             </p>
             <p>
-              안정적인 배포를 위해, main-trial-dev의 세 줄기로 구분하는 브랜치
-              관리 방법을 도입했습니다.
+              안정적인 배포를 위해, main-trial-dev 형식의 브랜치 관리 방법을
+              도입했습니다.
             </p>
             <p>
-              엑셀 기반 언어 번역을 진행하며 번역 결과물과 코드 내부 json 파일을
+              언어 번역 시 엑셀 형태의 번역 결과물과 코드 내부 json 파일을
               양방향으로 변환하기 위한 python tool을 개발했습니다.
             </p>
             <p>정기 배포 13회(22년 4회, 23년 4회, 24년 5회)</p>
@@ -95,22 +93,23 @@ function App() {
           <div className="sub-content">
             <h5>IVAS 프론트엔드 담당자</h5>
             <p>
-              고객사의 안정적인 분석을 위한 IVAS 솔루션에서 특정 고객사의
-              요구사항을 위한 기능을 추가하고 공통적으로 사용하기 위해
-              컴포넌트화 했습니다.
+              시스템 연동 분석 포탈 IVAS에서 고객사의 요구사항을 바탕으로 기능을
+              추가하고 <br />
+              서비스 공통적으로 적용하기 위해 컴포넌트화 했습니다.
             </p>
             <p>2개 고객사 담당(삼성카드, LG에너지솔루션)</p>
             <p>사용 기술 : Typescript, Next.js, tailwind, yarn berry</p>
             <p>특이사항 : monorepo 사용</p>
           </div>
           <div className="sub-content">
-            <h5>래브라도 & IVAS 기능 기획 담당자</h5>
+            <h5>래브라도 & IVAS 기획 담당자</h5>
             <p>
-              래브라도 또는 IVAS에 기능을 추가할 경우 요구사항 분석 및 Figma를
-              통해 기능을 기획했습니다.
+              래브라도, IVAS에 기능 추가 시 요구사항을 분석하고 Figma를 통해
+              UI를 기획했습니다.
             </p>
             <p>
-              외부 업체와 협력하여 기획할 때에도 연구소의 소통을 담당했습니다.
+              외부 업체와 협력하여 기획할 때에도 협력사 소통을 담당하고
+              있습니다.
             </p>
             <p>사용 기술 : Figma, powerpoint, Confluence, notion</p>
           </div>
